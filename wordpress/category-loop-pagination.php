414fda4a61b047ea6f3479e884484c839d38bdb3
Jayson <jaysonalipala15@gmail.com>
2014-03-25 15:35:35 +0800

    Remove the comments

--- wordpress/category-loop-pagination.php
+++ wordpress/category-loop-pagination.php
@@ -3,9 +3,7 @@
 <?php
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;                           
 query_posts( 'post_type=documents&category_name=by-customers&paged='.$paged );
-query_posts( 'post_type=post&category_name=by-customers&paged='.$paged );						   
-//query_posts( 'post_type=documents&paged='.$paged );
-//query_posts( 'post_type=post&paged='.$paged );
+query_posts( 'post_type=post&category_name=by-customers&paged='.$paged );
 if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <li>
